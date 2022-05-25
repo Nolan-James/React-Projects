024136d79fc81b9631d93e3f284d1854ce683858
Nolan-James <nj@nolanjames.me>
2022-05-25 13:40:33 +0100

    strapi backend

--- events/events-client/components/Layout.js
+++ events/events-client/components/Layout.js
@@ -1,19 +1,24 @@
 import Head from 'next/head';
+import { useRouter } from "next/router";
 import styles from '../styles/Layout.module.css'
 import Header from "./Header";
 import Footer from "./Footer";
+import Showcase from "@/components/Showcase";
 
 const Layout = ({ title, keywords, description, children }) => {
+    const router = useRouter();
+
     return (
         <div>
             <Head>
                 <title>{title}</title>
                 <meta name='description' content={description}/>
                 <meta name='keywords' content={keywords}/>
-
             </Head>
-
             <Header/>
+            {
+                router.pathname === '/' && <Showcase/>
+            }
             <div className={styles.container}>
                 {children}
             </div>
